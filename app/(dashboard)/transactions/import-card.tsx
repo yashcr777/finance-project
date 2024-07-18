114eaa7750804912c695e4e5bd7c081e2687435c
yashcr777 <yashagarwalnobody@gmail.com>
2024-07-19 00:32:47 +0530

    neww-commitrrrrrrr

---
+++ app/(dashboard)/transactions/import-card.tsx
@@ -0,0 +1,19 @@
+
+type Props={
+    data:string[][];
+    onCancel:()=>void;
+    onSubmit:(data:any)=>void;
+}
+
+
+export const ImportCard=({
+    data,
+    onCancel,
+    onSubmit
+}: Props)=>{
+    return(
+        <div>
+            Import Card
+        </div>
+    );
+};
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -14,8 +14,44 @@ import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS{
+    LIST="LIST",
+    IMPORT="IMPORT"
+};
+
+const INTIAL_IMPORT_RESULTS={
+    data:[],
+    errors:[],
+    meta:{},
+};
+
+
+
+
+
+
 
 const TransactionsPage=()=>{
+    const [variant,setVariant]=useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults]=useState(INTIAL_IMPORT_RESULTS);
+
+    const onUpload=(results:typeof INTIAL_IMPORT_RESULTS)=>{
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    };
+
+    const onCancelImport=()=>{
+        setImportResults(INTIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    }
+
+
+
+
     const newTransaction=useNewTransaction();
     const deleteTransactions=useBulkDeleteTransactions();
     const transactionsQuery=useGetTransactions();
@@ -42,6 +78,18 @@ const TransactionsPage=()=>{
         )
     }
 
+    if(variant===VARIANTS.IMPORT){
+        return (
+            <>
+                <ImportCard 
+                data={importResults.data}
+                onCancel={onCancelImport}
+                onSubmit={()=>{}}
+                />
+            </>
+        )
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -49,14 +97,17 @@ const TransactionsPage=()=>{
                     <CardTitle className="text-xl line-clamp-1">
                     transaction History
                     </CardTitle>
+                    <div className="flex items-center gap-x-2">
                     <Button onClick={newTransaction.onOpen} size="sm">
                         <Plus className="size-4 mr-2"/>
                         Add New
                     </Button>
+                    <UploadButton onUpload={onUpload}/>
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable 
-                    filterKey="name"
+                    filterKey="payee"
                     columns={columns} 
                     data={transactions} 
                     onDelete={(row)=>{
