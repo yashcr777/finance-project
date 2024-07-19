e5bb65184283e747af03fc72b13f95a5a0720793
yashcr777 <yashagarwalnobody@gmail.com>
2024-07-19 15:01:26 +0530

    neeee-commitwerrrrrrrrrr

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -7,6 +7,9 @@ import {
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { ImportTable } from "./import-table";
+import { CloudFog } from "lucide-react";
+import { convertAmountToMiliunits } from "@/lib/utils";
+import {format,parse} from "date-fns"
 
 const dateFormat="yyyy-MM-dd HH:mm:ss"
 const outputFormat="yyyy-MM-dd";
@@ -58,6 +61,47 @@ export const ImportCard=({
     }
     const progress=Object.values(selectedColumns).filter(Boolean).length;
 
+    const handleContinue=()=>{
+        
+        const getColumnIndex=(column:string)=>{
+            return column.split("_")[1];
+        };
+
+        const mappedData={
+            headers:headers.map((_header,index)=>{
+                const columnIndex=getColumnIndex(`column_${index}`);
+                return selectedColumns[`column_${columnIndex}`] || null;
+            }),
+            body:body.map((row)=>{
+                const transformedRow=row.map((cell,index)=>{
+                    const columnIndex=getColumnIndex(`column_${index}`);
+                    return selectedColumns[`column_${columnIndex}`]?cell:null;
+                });
+                return transformedRow.every((item)=>item===null)?
+                []
+                : transformedRow;
+            }).filter((row)=>row.length>0),
+        };
+        
+
+        const arrayOfData=mappedData.body.map((row)=>{
+            return row.reduce((acc:any,cell,index)=>{
+                const header=mappedData.headers[index];
+                if(header!==null){
+                    acc[header]=cell;
+                }
+                return acc;
+            },{});
+        });
+        const formattedData=arrayOfData.map((item)=>({
+            ...item,
+            amount:convertAmountToMiliunits(parseFloat(item.amount)),
+            date:format(parse(item.date,dateFormat,new Date()),outputFormat)
+        }));
+
+        onSubmit(formattedData);
+    };
+
     return(
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -69,7 +113,7 @@ export const ImportCard=({
                     <Button onClick={onCancel} size="sm" className="w-full lg:w-auto">
                         Cancel
                     </Button>
-                    <Button size="sm" disabled={progress<requiredOptions.length} onClick={()=>{}} className="w-full lg:w-auto">
+                    <Button size="sm" disabled={progress<requiredOptions.length} onClick={handleContinue} className="w-full lg:w-auto">
                         Continue ({progress}/{requiredOptions.length})
                     </Button>
                     </div>
