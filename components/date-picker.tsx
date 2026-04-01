b81453ff778a58af33903f5e4e40f10c350209f3
yashcr777 <153800267+yashcr777@users.noreply.github.com>
2026-04-01 16:31:16 +0530

    Refactor DatePicker component to manage open state

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -1,52 +1,52 @@
+"use client";
+
 import * as React from "react";
-import {format} from "date-fns";
+import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { SelectSingleEventHandler } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Calendar } from "./ui/calendar";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger
-} from "@/components/ui/popover"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
-type Props={
-    value?:Date;
-    onChange?:SelectSingleEventHandler;
-    disabled?:boolean;
-}
+type Props = {
+  value?: Date;
+  onChange?: SelectSingleEventHandler;
+  disabled?: boolean;
+};
 
-export const DatePicker=({
-    value,
-    onChange,
-    disabled
-}:Props)=>{
-    return (
-    <Popover>
-        <PopoverTrigger asChild>
-            <Button 
-            disabled={disabled}
-            variant="outline"
-            className={cn("w-full justify-start text-left font-normal",
-            !value && "text-muted-foreground",)}
-            >
-                <CalendarIcon className="size-4 mr-2"/>
-                {value?format(value,"PPP"):<span>Pick a Date</span>}
-            </Button>
-        </PopoverTrigger>
-        <PopoverContent>
-            <Calendar
-            mode="single"
-            selected={value}
-            onSelect={onChange}
-            disabled={disabled}
-            initialFocus
-            fromYear={1990}
-            toYear={new Date().getFullYear() + 10}
-            />
-        </PopoverContent>
+export const DatePicker = ({ value, onChange, disabled }: Props) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          disabled={disabled}
+          variant="outline"
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !value && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="size-4 mr-2" />
+          {value ? format(value, "PPP") : <span>Pick a Date</span>}
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={(day, selectedDay, modifiers, event) => {
+            onChange?.(day, selectedDay, modifiers, event);
+            setOpen(false); // close after selecting a date
+          }}
+          disabled={disabled}
+          initialFocus
+        />
+      </PopoverContent>
     </Popover>
-    );
+  );
 };
